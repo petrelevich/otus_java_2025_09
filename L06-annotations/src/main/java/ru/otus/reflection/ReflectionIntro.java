48f09c5c79d65031bfc4e12d80880fa5ffe2eb11
spv <pvstrekalov@yandex.ru>
2025-10-21 19:39:32 +0400

    L06-annotations

--- L06-annotations/src/main/java/ru/otus/reflection/ReflectionIntro.java
+++ L06-annotations/src/main/java/ru/otus/reflection/ReflectionIntro.java
@@ -9,26 +9,26 @@ public class ReflectionIntro {
 
     public static void main(String[] args) throws ClassNotFoundException {
         // Получение Class у класса
-        Class<ReflectionIntro> clazz1 = ReflectionIntro.class;
+        var clazz1 = ReflectionIntro.class;
         log.info("simpleName_1:{}", clazz1.getSimpleName());
 
         // Получение Class у экземпляра
         var app = new ReflectionIntro();
-        Class<? extends ReflectionIntro> clazz2 = app.getClass();
+        var clazz2 = app.getClass();
         log.info("simpleName_2:{}", clazz2.getSimpleName());
 
         // Получение Class через строковое имя класса
         // Например, для создания экземпляров классов, отсутствующих при компиляции (JDBC-драйвер)
-        Class<?> classString = Class.forName("java.lang.String");
+        var classString = Class.forName("java.lang.String");
         log.info("simpleName_3:{}", classString.getSimpleName());
         log.info("canonicalName:{}", classString.getCanonicalName());
 
         // Получение Class у примитива
-        Class<Integer> classInt = int.class;
+        var classInt = int.class;
         log.info("TypeName int:{}", classInt.getTypeName());
 
         // Получение Class у обёртки примитива
-        Class<Integer> classInteger = Integer.class;
+        var classInteger = Integer.class;
         log.info("TypeName Integer:{}", classInteger.getTypeName());
     }
 }
