0231f756ca310ae05a6c7da61807df7a649279dd
spv <pvstrekalov@yandex.ru>
2025-10-31 20:11:55 +0400

    L08-gc

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,8 @@ include("L03-qa")
 include("L04-generics")
 include("L05-collections")
 include("L06-annotations")
-include("L08-gc")
+include("L08-gc:demo")
+include("L08-gc:homework")
 include("L10-byteCodes")
 
 pluginManagement {
