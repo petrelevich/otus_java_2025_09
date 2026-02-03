37ed59215f42bd547e33da3768593b0cf1c16227
spv <pvstrekalov@yandex.ru>
2026-02-03 19:07:17 +0400

    L27-websocket

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -44,7 +44,12 @@ include("L25-di:homework-template")
 
 include("L26-springBootMvc")
 
-include ("L28-springDataJdbc")
+include("L27-websocket:websocket")
+include("L27-websocket:messager")
+include("L27-websocket:messager-starter")
+include("L27-websocket:application")
+
+include("L28-springDataJdbc")
 
 pluginManagement {
     val jgitver: String by settings
